fe8bcd28285ae0a0475745a12fbeb246d7837d8b
princepanchal1906 <princepanchal2002@gmail.com>
2026-04-20 15:38:34 +0530

    first commit

---
+++ src/Layout.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+export default function Layout() {
+  return (
+    <div className="portfolio-container min-h-screen">
+      {/* Background elements */}
+      <div className="bg-blob blob-1"></div>
+      <div className="bg-blob blob-2"></div>
+
+      <nav className="navbar">
+        <div className="nav-brand">
+          <Link to="/">
+            <span className="gradient-text">Portfolio.</span>
+          </Link>
+        </div>
+        <div className="nav-links">
+          <Link to="/about">About</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </nav>
+
+      <div className="pt-24">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
