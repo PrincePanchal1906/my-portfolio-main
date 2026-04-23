b6723cdd212d51af6bfd7fedbacfd573ef9021bc
princepanchal1906 <princepanchal2002@gmail.com>
2026-04-23 17:46:24 +0530

    remove .env

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.error("Could not connect to MongoDB", err));
+.catch((err) => console.error("MongoDB error :", err));
 
 app.use("/api", contactRoutes);
 
