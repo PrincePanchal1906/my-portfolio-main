dd8453e10a2eee43820b5d8450f74214ff7389a0
princepanchal1906 <princepanchal2002@gmail.com>
2026-04-23 18:18:06 +0530

    check post req

--- backend/controllers/contact.js
+++ backend/controllers/contact.js
@@ -7,8 +7,9 @@ export async function getContact(req, res) {
     if (!name || !email || !message) {
         return res.status(400).json({ error: "All fields are required" });
     }
-    console.log("Received contact data:", { name, email, message });
     const contactData = await Contact.create({ name, email, message });
+
+console.log("✅ Saved Data:", contactData);
     res.status(201).json({ message: "Contact received successfully" });
     
   } catch (error) {
