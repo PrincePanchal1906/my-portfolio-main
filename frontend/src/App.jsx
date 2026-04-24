c7f3656f3695be6235d1e62cd0d50fc26bb2abc2
princepanchal1906 <princepanchal2002@gmail.com>
2026-04-24 15:39:03 +0530

    check gtag with main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,25 @@ import Home from "./Home";
 import About from "./About";
 import Projects from "./Projects";
 import Contact from "./Contact";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    const script1 = document.createElement("script");
+    script1.async = true;
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-C9QDC9XQ9Q";
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      window.gtag = gtag;
+      gtag('js', new Date());
+      gtag('config', 'G-C9QDC9XQ9Q');
+    `;
+    document.head.appendChild(script2);
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
